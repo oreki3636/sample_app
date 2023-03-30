2c9fc8d0f5e742eb9fe4f6aba38493ea36501c7d
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-03-30 03:26:23 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,14 +11,16 @@ class ListsController < ApplicationController
    # 3. データをデータベースに保存するためのsaveメソッド実行
    list.save
    # 4. トップ画面へリダイレクト
-   redirect_to '/top'
-  end
-  
-  def show
-    @list = List.find(params[:id])  
+   # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+   redirect_to list_path(list.id)
   end
 
-  
+  def show
+    @list = List.find(params[:id])
+  end
+
+
   def index
     @lists=List.all
   end
